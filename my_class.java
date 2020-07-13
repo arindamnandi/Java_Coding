bc0f919abcdc54417080b7fdfb37d33708ae24cb
arindamnandi <45317918+arindamnandi@users.noreply.github.com>
2020-07-13 22:20:17 +0530

    A procedural technique and calculate ratio

---
+++ my_class.java
@@ -0,0 +1,19 @@
+// A procedural technique
+public class my_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int chainring = 52;
+		int cog = 11;
+		double ratio = ((double) chainring )/ cog;
+		System.out.println("Ratio is :" + ratio);
+
+		chainring = 30;
+		cog = 27;
+		ratio = ((double) chainring )/ cog;
+		System.out.println("Ratio is :" + ratio);
+		
+		
+	}
+
+}
